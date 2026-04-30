15d3956c270e555595cd1677a5584d71144158f0
Joselyn <joselyntellozavala@gmail.com>
2026-04-29 20:02:42 -0500

    Agregar HashMap y Streams en la clase Inventario

--- Joselyn_Tello_escaperoom/src/Inventario.java
+++ Joselyn_Tello_escaperoom/src/Inventario.java
@@ -1,19 +1,23 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class Inventario {
 
     private ArrayList<Item> items;
+    private HashMap<String, Item> itemsPorNombre;
     private int capacidadMax;
 
     public Inventario(int capacidadMax) {
         this.items = new ArrayList<>();
+        this.itemsPorNombre = new HashMap<>();
         this.capacidadMax = capacidadMax;
     }
-
 
     public boolean agregarItem(Item item) {
         if (items.size() < capacidadMax) {
             items.add(item);
+            itemsPorNombre.put(item.getNombre(), item);
             System.out.println("Has guardado [" + item.getNombre() + "] en tu inventario.");
             return true;
         } else {
@@ -22,15 +26,28 @@ public class Inventario {
         }
     }
 
+
+    public Item buscarItem(String nombre) {
+        return itemsPorNombre.getOrDefault(nombre, null);
+    }
+
+
     public void mostrarInventario() {
         System.out.println("--- INVENTARIO ---");
         if (items.isEmpty()) {
             System.out.println("No tienes ningún objeto en el inventario");
         } else {
-            for (int i = 0; i < items.size(); i++) {
-                System.out.println(i + ". " + items.get(i).getNombre() + " - " + items.get(i).getDescripcion());
-            }
+            items.stream()
+                    .forEach(item -> System.out.println("• " + item.getNombre() + " - " + item.getDescripcion()));
         }
+    }
+
+
+    public void mostrarPistas() {
+        System.out.println("--- PISTAS EN INVENTARIO ---");
+        items.stream()
+                .filter(item -> item.getAccion() == AccionItem.LEER_PISTA)
+                .forEach(item -> System.out.println("📜 " + item.getNombre() + ": " + item.getDescripcion()));
     }
 
     public boolean estaVacio() {
